94ccb16d2076d45f95dabebeb0fe3adeda7edbc1
Ludwig Bäcklund <ludwigbacklund@gmail.com>
2023-04-03 21:18:58 +0900

    Initial commit

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    browser: {
+      enabled: true,
+      name: "webkit",
+      provider: "playwright",
+    },
+  },
+});
